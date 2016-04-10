632044acd95639c1de9c381d01b638c095d8e3d4
Christian Dürnberger <chris_d85@hotmail.com>
2016-04-10 14:59:36 +0200

    1.0.0

---
+++ source/deviceInstaller/deviceInstaller.cpp
@@ -0,0 +1,22 @@
+
+#include "deviceInstaller/deviceInstaller.h"
+
+
+namespace RaumserverInstaller
+{
+    namespace DeviceInstaller
+    {
+
+        DeviceInstaller::DeviceInstaller() : RaumserverInstallerBase()
+        {
+        }
+
+
+        DeviceInstaller::~DeviceInstaller()
+        {
+        }
+
+ 
+
+    }
+}
---
+++ source/deviceInstaller/deviceInstaller_RF.cpp
@@ -0,0 +1,22 @@
+
+#include "deviceInstaller/deviceInstaller_RF.h"
+
+
+namespace RaumserverInstaller
+{
+    namespace DeviceInstaller
+    {
+
+        DeviceInstaller_RaumfeldDevice::DeviceInstaller_RaumfeldDevice() : DeviceInstaller()
+        {
+        }
+
+
+        DeviceInstaller_RaumfeldDevice::~DeviceInstaller_RaumfeldDevice()
+        {
+        }
+
+
+
+    }
+}
